b272925b03c7bd5e11177cb0599f14c8331775df
Yelitza Cho <yelitza.choque@gmail.com>
2017-07-12 07:40:50 -0500

    Avanzado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-$(document).foundation()
+$(document).foundation();
+
+$(document).ready(function() {
+  $('.hamburguer-btn').click(function() {
+    $(this).toggleClass('on');
+  });
+});
+
+function animateHamburger() {
+  $('.hamburguer-btn').toggleClass('open');
+}
+function slideMenu(){
+  $('.navbar').toggleClass('open');
+}
+
+$('.hamburguer-btn').click(function(){
+  animateHamburger();
+  slideMenu();
+});
